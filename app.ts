886e1f6b5ed7fa21629cd68e5610c93ad29170e3
x-rodionov <59539386+x-rodionov@users.noreply.github.com>
2021-07-07 23:25:11 +0600

    db client naming fixed

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
 import * as flags from "https://deno.land/std/flags/mod.ts";
-import { DbClient } from "https://deno.land/x/postgres/mod.ts";
+import { Client } from "https://deno.land/x/postgres/mod.ts";
 
 const DEFAULT_PORT = 8080;
 const argPort = flags.parse(Deno.args).port;
@@ -8,7 +8,7 @@ const port = argPort ? Number(argPort) : DEFAULT_PORT;
 
 const app = new Application();
 const router = new Router();
-const db = new DbClient(Deno.env.get("DATABASE_URL"));
+const db = new Client(Deno.env.get("DATABASE_URL"));
 router
   .get("/ping", (ctx) => {
     ctx.response.body = `Hello Knyte World! Deno ${Deno.version.deno} is in charge!\n`;
