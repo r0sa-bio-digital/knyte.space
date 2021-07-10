dc90747f7f29cdc62ad94018bcd1adb9582e329d
x-rodionov <code512edu@gmail.com>
2021-07-11 02:20:23 +0300

    Revert "test connection simplified"
    This reverts commit 957cad92e97029936604359a1f32c1c4fcbef3e5.

--- index.js
+++ index.js
@@ -19,9 +19,19 @@ router.get('/', async (ctx) => {
     };
 });
 router.get('/knytes', async (ctx) => {
-    await pgClient.connect();
+    try {
+        await pgClient.connect();
+        console.log('connected');
+    } catch(e) {
+        console.warn(e);
+    }
     const result = await pgClient.query('SELECT NOW()');
-    await pgClient.end();
+    try {
+        await pgClient.end();
+        console.log('disconnected');
+    } catch(e) {
+        console.warn(e);
+    }
     ctx.body = {result};
 });
 
