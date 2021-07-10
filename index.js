558548e4f0d83582e63d82de7297bf4e080a78b6
x-rodionov <code512edu@gmail.com>
2021-07-11 02:48:35 +0300

    pg methods generalised

--- index.js
+++ index.js
@@ -7,68 +7,47 @@ const app = new Koa();
 const PORT = process.env.PORT || 3000;
 const router = new Router();
 
+async function runQuery(queryString) {
+    const pgClient = new PG.Client({
+        connectionString,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });    
+    try {
+        await pgClient.connect();
+        console.log('connected');
+    } catch(e) {
+        console.warn(e);
+    }
+    let result = {};
+    try {
+        result = (await pgClient.query(queryString)).rows;
+        console.log('query done: ' + queryString);
+    } catch (e) {
+        console.warn(e);
+    }
+    try {
+        await pgClient.end();
+        console.log('disconnected');
+    } catch(e) {
+        console.warn(e);
+    }
+    return result;
+}
+
 router.get('/', async (ctx) => {
         ctx.body = {
         message: 'hello world from knyte.space!'
     };
 });
 router.get('/now', async (ctx) => {
-    const pgClient = new PG.Client({
-        connectionString,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    });    
-    try {
-        await pgClient.connect();
-        console.log('connected');
-    } catch(e) {
-        console.warn(e);
-    }
-    let result = {};
-    try {
-        const queryString = 'SELECT NOW()';
-        result = (await pgClient.query(queryString)).rows;
-        console.log('query done: ' + queryString);
-    } catch (e) {
-        console.warn(e);
-    }
-    try {
-        await pgClient.end();
-        console.log('disconnected');
-    } catch(e) {
-        console.warn(e);
-    }
-    ctx.body = {result};
+    const queryString = 'SELECT NOW()';
+    ctx.body = {result: await runQuery(queryString)};
 });
 router.get('/knytes', async (ctx) => {
-    const pgClient = new PG.Client({
-        connectionString,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    });    
-    try {
-        await pgClient.connect();
-        console.log('connected');
-    } catch(e) {
-        console.warn(e);
-    }
-    let result = {};
-    try {
-        const queryString = 'SELECT * FROM "public"."knytes" ORDER BY "knyte_id"';
-        result = (await pgClient.query(queryString)).rows;
-        console.log('query done: ' + queryString);
-    } catch (e) {
-        console.warn(e);
-    }
-    try {
-        await pgClient.end();
-        console.log('disconnected');
-    } catch(e) {
-        console.warn(e);
-    }
-    ctx.body = {result};
+    const queryString = 'SELECT * FROM "public"."knytes" ORDER BY "knyte_id"';
+    ctx.body = {result: await runQuery(queryString)};
 });
 
 app.use(router.routes());
